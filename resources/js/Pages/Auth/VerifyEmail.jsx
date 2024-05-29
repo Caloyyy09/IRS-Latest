6a9d76b5aeb6ada357332fa0bf36c478b794bbae
Caloyyy09 <tenkenkou@gmail.com>
2024-05-29 11:54:16 +0800

    First commit

--- resources/js/Pages/Auth/VerifyEmail.jsx
+++ resources/js/Pages/Auth/VerifyEmail.jsx
@@ -1,45 +1,52 @@
+import React from 'react';
+import { Head, Link, useForm } from '@inertiajs/react';
 import GuestLayout from '@/Layouts/GuestLayout';
 import PrimaryButton from '@/Components/PrimaryButton';
-import { Head, Link, useForm } from '@inertiajs/react';
+import logoImg from '@/Components/logo.png';
 
 export default function VerifyEmail({ status }) {
     const { post, processing } = useForm({});
 
     const submit = (e) => {
         e.preventDefault();
-
         post(route('verification.send'));
     };
 
     return (
         <GuestLayout>
             <Head title="Email Verification" />
+            
+            <div className="flex items-center justify-center mt-24">
+                <div className="w-96 h-auto p-9 ">
+                    <div className="flex justify-center mb-4">
+                        <img src={logoImg} alt="Logo"className="w-40 h-auto"  />
+                    </div>
+                    <div className="mb-4 text-sm text-gray-600 text-justify">
+                        Thanks for signing up! Before getting started, could you verify your email address by clicking on the
+                        link we just emailed to you? If you didn't receive the email, we will gladly send you another.
+                    </div>
 
-            <div className="mb-4 text-sm text-gray-600">
-                Thanks for signing up! Before getting started, could you verify your email address by clicking on the
-                link we just emailed to you? If you didn't receive the email, we will gladly send you another.
+                    {status === 'verification-link-sent' && (
+                        <div className="mb-4 font-medium text-sm text-green-600">
+                            A new verification link has been sent to the email address you provided during registration.
+                        </div>
+                    )}
+
+                    <form onSubmit={submit}>
+                        <div className="mt-4 flex flex-col items-center">
+                            <Link
+                                href={route('logout')}
+                                method="post"
+                                as="button"
+                                className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mb-4"
+                            >
+                                Log Out
+                            </Link>
+                            <PrimaryButton disabled={processing}>Resend Verification Email</PrimaryButton>
+                        </div>
+                    </form>
+                </div>
             </div>
-
-            {status === 'verification-link-sent' && (
-                <div className="mb-4 font-medium text-sm text-green-600">
-                    A new verification link has been sent to the email address you provided during registration.
-                </div>
-            )}
-
-            <form onSubmit={submit}>
-                <div className="mt-4 flex items-center justify-between">
-                    <PrimaryButton disabled={processing}>Resend Verification Email</PrimaryButton>
-
-                    <Link
-                        href={route('logout')}
-                        method="post"
-                        as="button"
-                        className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    >
-                        Log Out
-                    </Link>
-                </div>
-            </form>
         </GuestLayout>
     );
 }
